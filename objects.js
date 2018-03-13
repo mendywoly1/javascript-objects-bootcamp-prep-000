693481d1479e8f0c9c022b9cffd98c2578c954fd
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-03-13 09:00:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist, {[artistName] : songTitle})
 }
 
-function removeFromPlaylist(playlist, artistName){
+function removeFromPlaylist(playlist, artistName) {
    delete playlist.artistName;
     playlist;
 }
